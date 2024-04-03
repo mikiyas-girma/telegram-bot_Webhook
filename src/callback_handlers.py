cb3b3cb23c88b7cf8888a7736f35c9e4a8415b9e
Mikiyas Girma <mikiyasgirmaet@gmail.com>
2024-04-04 02:45:38 +0300

    created inline keyboard & handler callback_query

--- src/callback_handlers.py
+++ src/callback_handlers.py
@@ -1,30 +1,47 @@
 from main_bot import bot
-import telebot
+from telebot.types import (InlineKeyboardMarkup,
+                           InlineKeyboardButton,
+                           SwitchInlineQueryChosenChat)
+from telebot.util import quick_markup
 
 
-# Handler for /confirm command
+@bot.message_handler(commands=['social'])
+def list_social_media(message):
+    social = quick_markup({
+        'Chapa': {'pay': True},
+        'Twitter': {'url': 'https://twitter.com'},
+        'Facebook': {'url': 'https://facebook.com'},
+        'Back': {'callback_data': 'whatever'}
+    }, row_width=2)
+
+    bot.send_message(message.chat.id, 'social medias', reply_markup=social)
+
+
 @bot.message_handler(commands=['confirm'])
 def confirm_command(message):
-    keyboard = telebot.types.InlineKeyboardMarkup()
-    button_confirm = telebot.types.InlineKeyboardButton(
-        'Confirm',
-        callback_data='confirm')
-    keyboard.row(button_confirm)
+
+    markup = InlineKeyboardMarkup()
+    markup.row_width = 1
+
+    markup.add(InlineKeyboardButton(
+        'admin',
+        switch_inline_query_chosen_chat=SwitchInlineQueryChosenChat(
+            'admin', allow_user_chats=True)),
+
+               InlineKeyboardButton('confirm', callback_data='confirm'),
+               InlineKeyboardButton('cancel', callback_data='cancel'))
     bot.send_message(message.chat.id, "Are you sure you want to confirm?",
-                     reply_markup=keyboard)
+                     reply_markup=markup)
 
 
-# Handler for inline callback query
 @bot.callback_query_handler(func=lambda call: True)
 def handle_callback_query(call):
-    # Extract information from the callback query
     callback_data = call.data
-    chat_id = call.message.chat.id
+    callback_sender = call.from_user
 
-    # Perform custom logic based on the callback data
     if callback_data == 'confirm':
-        bot.send_message(chat_id, "You confirmed!")
-        bot.answer_callback_query(call.id)
+        bot.answer_callback_query(call.id, text="confirmed", show_alert=True)
     elif callback_data == 'cancel':
-        bot.send_message(chat_id, "You canceled!")
-        bot.answer_callback_query(call.id)
+        bot.answer_callback_query(
+            call.id,
+            f"{callback_sender.first_name} you have cancelled it")
